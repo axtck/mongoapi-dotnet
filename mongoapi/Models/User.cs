b81e513749cb1eb7665ac9f95b6aba49646a62f5
Alexander Tack <alexander.tack@student.vives.be>
2021-03-30 18:21:37 +0200

    Add mongo and model

---
+++ mongoapi/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mongoapi.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] // bson ObjectId
+        public string Id { get; set; }
+
+        // props
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
