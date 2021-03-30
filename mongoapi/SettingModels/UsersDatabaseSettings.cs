a754491e3df4c9f4d962b18c1e793dec3491961b
Alexander Tack <alexander.tack@student.vives.be>
2021-03-30 18:33:39 +0200

    Add databasesetting file

---
+++ mongoapi/SettingModels/UsersDatabaseSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mongoapi.SettingModels
+{
+    // class used for storing appsettings.json file's UsersDatabaseSettings property values
+    public class UsersDatabaseSettings : IUsersDatabaseSettings
+    {
+        public string UsersCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IUsersDatabaseSettings
+    {
+        string UsersCollectionName { get; set; } // collection name
+        string ConnectionString { get; set; } // connection string
+        string DatabaseName { get; set; } // db name
+    }
+}
